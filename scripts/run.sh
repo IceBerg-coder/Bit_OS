e3d64aee0f16aa3e461f90498478d7683ccb9304
IceBerg-coder <bergice169@gmail.com>
2026-03-02 16:56:04 +0800

    Add port 8443->443 HTTPS forwarding to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Consolidated launch script for BitOS
 # Port mappings:
-#   host:2222 -> BitOS:22  (SSH  - ssh root@localhost -p 2222)
+#   host:2222 -> BitOS:22  (SSH   - ssh root@localhost -p 2222)
 #   host:2323 -> BitOS:23  (Telnet)
-#   host:8180 -> BitOS:80  (HTTP - http://localhost:8180)
+#   host:8180 -> BitOS:80  (HTTP  - http://localhost:8180)
+#   host:8443 -> BitOS:443 (HTTPS - https://localhost:8443)
 
 STORAGE_IMG="output/storage.img"
 [ ! -f "$STORAGE_IMG" ] && echo "[!] No storage.img found. Run: bash scripts/create_storage.sh" && STORAGE_IMG=""
@@ -12,5 +13,5 @@ qemu-system-x86_64 \
     -cdrom output/bitos.iso \
     -m 256M \
     -net nic \
-    -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::2323-:23,hostfwd=tcp::8180-:80 \
+    -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::2323-:23,hostfwd=tcp::8180-:80,hostfwd=tcp::8443-:443 \
     ${STORAGE_IMG:+-drive file=$STORAGE_IMG,format=raw,if=virtio}
