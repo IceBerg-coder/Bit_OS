b3e2acf2c9aa3084cfe23e25b6719e86aeab35de
IceBerg-coder <bergice169@gmail.com>
2026-03-02 14:24:11 +0800

    Update run.sh: ISO boot + SSH/telnet/http port forwarding

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-# consolidated launch script for BitOS
+# Consolidated launch script for BitOS
 
-# Ensure we have a storage disk
-if [ ! -f "output/storage.img" ]; then
-    echo "Creating new 64MB storage disk..."
-    dd if=/dev/zero of=output/storage.img bs=1M count=64
-    mkfs.ext4 output/storage.img
-fi
-
-# Rebuild the image
-./scripts/create_image.sh
-
-# Run QEMU
+# Run QEMU with ISO boot and port forwarding
+# Port mappings:
+#   host:2222 -> BitOS:22  (SSH  - ssh root@localhost -p 2222)
+#   host:2323 -> BitOS:23  (Telnet)
+#   host:8180 -> BitOS:80  (HTTP - http://localhost:8180)
 qemu-system-x86_64 \
-    -kernel output/vmlinuz \
-    -initrd output/initramfs.cpio.gz \
-    -append "console=ttyS0 quiet" \
-    -nographic \
-    -net nic -net user \
+    -cdrom output/bitos.iso \
+    -m 256M \
+    -net nic \
+    -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::2323-:23,hostfwd=tcp::8180-:80
     -drive file=output/storage.img,format=raw,if=virtio
